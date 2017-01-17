915723bae0ec3a7511dad66d9467f2a3176ad23a
Margus Pärt <margus@wave.ee>
2017-01-17 11:43:14 +0200

    save

--- specification/run_specs_remote.sh
+++ specification/run_specs_remote.sh
@@ -5,6 +5,6 @@ git commit -a --message="save"
 git push origin master
 
 ssh margusp@yo <<"EOF"
-cd $CMSSW_BASE/src/tthAnalysis/HiggsToTauTau/specification
+cd /home/margusp/VHbbNtuples_7_6_x/CMSSW_7_6_3/src/tthAnalysis/HiggsToTauTau/specification
 bash run_specs.sh
 EOF
