f87baaab3cabed3ea4a6550557d48ce0eddf2229
Margus Pärt <margus@wave.ee>
2017-01-18 14:23:47 +0200

    save

--- specification/run_specs.py
+++ specification/run_specs.py
@@ -1,9 +1,17 @@
-
+import sys
 import specs
 import models
 
-spec_runner = models.SpecRunner(specs = specs.all_specs)
 
-spec_runner.run_specs()
+run_specific_test = sys.argv[1]
+
+spec_runner = models.SpecRunner(
+    specs = specs.all_specs
+    )
+
+if run_specific_test:
+    spec_runner.run_spec(run_specific_test)
+else:
+    spec_runner.run_specs()
 
 spec_runner.print_summary()
